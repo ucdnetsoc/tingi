944aefd0956195cb696e4baa74d64b313c097f08
dastronmighty <eoghan.hogan@ucdconnect.ie>
2019-02-19 08:27:05 +0000

    init commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//======<requirements>===========
+var ip = require('ip'),
+    express = require('express'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    expressSanitizer = require('express-sanitizer');
+//======</requirements>===========
+
+//=======<APP SETUP>========
+var app = express();
+
+app.set("view engine", "ejs"); //use  embeded-javascript
+
+app.use(express.static(__dirname + '/public')); //use public directory to server staic files
+
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // use body parser
+app.use(methodOverride("_method")); //override methods of form (put/delete)
+app.use(expressSanitizer());
+
+//=======</APP SETUP>========
+
+
+
+//any route we havent defined
+app.get("*", function (req, res) {
+    res.send("yo")
+});
+
+//port listener
+app.listen('3000', ip.address(), function () {
+    console.log("SERVER STARTED!!!");
+    var address = ip.address() + ":3000";
+    console.log("goto -> " + address);
+});
