84e411ecb932fe0a99254a24419e31d5495a3279
Oisin Quinn <oisin1001@gmail.com>
2019-02-25 18:04:28 +0000

    Added authentication component

---
+++ client/src/Auth.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+// import CardList from '../../components/CardList/CardList';
+// import Nav from '../../components/Nav/Nav';
+// import './Archive.css';
+// import '../../components/Footer/Footer.css'
+// import Twitter from '../../images/twitter.svg'
+// import Facebook from '../../images/facebook.svg'
+// import Logo from '../../components/Logo/Logo'
+
+class Auth extends Component {
+  render() {
+    return (
+      <div>
+          <p>Hello world</p>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    window.scrollTo(0, 0)
+  }
+}
+
+export default Auth;
